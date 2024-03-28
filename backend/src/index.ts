c754f6489c0537e622226b552c003716d2082090
spidey0p <jyotiradityabiswal64@gmail.com>
2024-03-29 00:49:17 +0530

    backend 2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,32 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign } from 'hono/jwt'
+import { sign, verify } from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string,
+		JWT_SECRET: string,
+	}
+}>();
+
+//middlewares
+app.use('/api/v1/blog/*', async (c, next) => {
+  const header = c.req.header("authorization") || "";
+  const token = header.split(" ")[1];
+
+  const response = await verify(header, c.env.JWT_SECRET);
+  if(response.id) {
+    next();
+  } else {
+    c.status(403)
+    return c.json({ error: "unauthorized"})
+  }
+})
 
 app.post('/api/v1/signup', async (c) => {
   const prisma = new PrismaClient({
-    //@ts-ignore
-    datasourceUrl: c.env.DATABASE_URL
+    datasourceUrl: c.env?.DATABASE_URL
 }).$extends(withAccelerate())
 
   const body = await c.req.json();
@@ -20,7 +38,6 @@ app.post('/api/v1/signup', async (c) => {
     },
   })
 
-  //@ts-ignore
   const token = await sign({id: user.id}, c.env.JWT_SECRET)
 
   return c.json({
@@ -28,9 +45,27 @@ app.post('/api/v1/signup', async (c) => {
   })
 })
 
-// app.post('/api/v1/signin', (c) => {
-//   return c.json({ message: 'Signin success' })
-// })
+app.post('/api/v1/signin', async (c) => {
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env?.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const body = await c.req.json();
+    const user = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+        password: body.password
+      }
+    });
+
+      if(!user) {
+        c.status(403);
+        return c.json({ message: "user not found" })
+      }
+
+      const jwt = await sign({id: user.id}, c.env.JWT_SECRET);
+      return c.json({ jwt });
+})
 
 // app.post('/api/v1/blog', (c) => {
 //   return c.json({ message: 'Vlog created' })
