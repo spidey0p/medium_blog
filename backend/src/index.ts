dfc39aaf5524ed57f052fcc528e36ce01660ae95
spidey0p <jyotiradityabiswal64@gmail.com>
2024-03-30 18:12:25 +0530

    migrations

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { sign, verify } from 'hono/jwt'
+import { decode, sign, verify } from 'hono/jwt'
 
 const app = new Hono<{
 	Bindings: {
@@ -10,73 +10,31 @@ const app = new Hono<{
 	}
 }>();
 
-//middlewares
-app.use('/api/v1/blog/*', async (c, next) => {
-  const header = c.req.header("authorization") || "";
-  const token = header.split(" ")[1];
-
-  const response = await verify(header, c.env.JWT_SECRET);
-  if(response.id) {
-    next();
-  } else {
-    c.status(403)
-    return c.json({ error: "unauthorized"})
-  }
-})
-
-app.post('/api/v1/signup', async (c) => {
+app.post('/api/v1/user/signup', async (c) => {
+  const body = await c.req.json()
   const prisma = new PrismaClient({
-    datasourceUrl: c.env?.DATABASE_URL
-}).$extends(withAccelerate())
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
 
-  const body = await c.req.json();
-
+  try {
   const user = await prisma.user.create({
     data: {
-      email: body.email,
-      password: body.password,
-    },
+      username: body.username,
+      password:  body.password,
+      name: body.name
+    }
   })
+  const jwt = await sign({
+    id: user.id
+  }, c.env.JWT_SECRET)
 
-  const token = await sign({id: user.id}, c.env.JWT_SECRET)
+  return c.text(jwt)
+  
+  } catch(e) {
+    c.status(411);
+    return c.text('User already exists')
+  }
 
-  return c.json({
-    jwt: token
-  })
 })
-
-app.post('/api/v1/signin', async (c) => {
-    const prisma = new PrismaClient({
-      datasourceUrl: c.env?.DATABASE_URL,
-    }).$extends(withAccelerate());
-
-    const body = await c.req.json();
-    const user = await prisma.user.findUnique({
-      where: {
-        email: body.email,
-        password: body.password
-      }
-    });
-
-      if(!user) {
-        c.status(403);
-        return c.json({ message: "user not found" })
-      }
-
-      const jwt = await sign({id: user.id}, c.env.JWT_SECRET);
-      return c.json({ jwt });
-})
-
-// app.post('/api/v1/blog', (c) => {
-//   return c.json({ message: 'Vlog created' })
-// })
-
-// app.put('/api/v1/blog', (c) => {
-//   return c.json({ message: 'Vlog updated' })
-// })
-
-// app.get('/api/v1/blog/:id', (c) => {
-//   return c.json({ message: 'Vlog fetched' })
-// })
 
 export default app
