13b7044e200f094c389cb27eb6f5b1d821540abc
spidey0p <jyotiradityabiswal64@gmail.com>
2024-04-08 20:48:18 +0530

    App bar

---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,38 @@
+import { Appbar } from "../components/Appbar"
+import { BlogCard } from "../components/BlogCard"
+import { useBlogs } from "../hooks"
+
+export const Blogs = () => {
+    const { loading, blogs } = useBlogs();
+
+    if (loading) {
+        return <div>
+            Loading...
+        </div>
+    }
+    return <div>
+        <Appbar />
+        <div className="flex justify-center">
+            <div className="max-w-xl">
+                <BlogCard
+                    authorName={"John Doe"}
+                    title={"How an Ugly Single-page Application Can Outperform a Beautifully Crafted Multi-page Application"}
+                    content={"This is to read the blog, you need to click on the title. This is to read the blog, you need to click on the title. This is to read the blog, you need to click on the title. This is to read the blog, you need to click on the title."}
+                    publishedDate={"2021-09-01"}
+                />
+                <BlogCard
+                    authorName={"John Doe"}
+                    title={"How an Ugly Single-page Application Can Outperform a Beautifully Crafted Multi-page Application"}
+                    content={"This is to read the blog, you need to click on the title. This is to read the blog, you need to click on the title. This is to read the blog, you need to click on the title. This is to read the blog, you need to click on the title."}
+                    publishedDate={"2021-09-01"}
+                />
+                <BlogCard
+                    authorName={"John Doe"}
+                    title={"How an Ugly Single-page Application Can Outperform a Beautifully Crafted Multi-page Application"}
+                    content={"This is to read the blog, you need to click on the title. This is to read the blog, you need to click on the title. This is to read the blog, you need to click on the title. This is to read the blog, you need to click on the title."}
+                    publishedDate={"2021-09-01"}
+                />
+            </div>
+        </div>
+    </div>
+}
