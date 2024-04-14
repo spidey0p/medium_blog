4bb13f486711f1b916a3058c53864c7d0f0f7bf4
spidey0p <jyotiradityabiswal64@gmail.com>
2024-04-14 22:29:15 +0530

    Refactor Blog component to fix loading and error handling

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,51 +1,3 @@
-// import { useBlog } from "../hooks";
-// import { useParams } from "react-router-dom";
-// import { FullBlog } from "../components/FullBlog";
-// import { Spinner } from "../components/Spinner";
-// import { Appbar } from "../components/Appbar";
-
-// export const Blog = () => {
-//     const { id } = useParams();
-//     const { loading, blog, error } = useBlog({
-//         id: id || ""
-//     });
-
-//     if (loading) {
-//         return (
-//             <div>
-//                 <Appbar />
-//                 <div className="h-screen flex flex-col justify-center">
-//                     <div className="flex justify-center">
-//                         <Spinner />
-//                     </div>
-//                 </div>
-//             </div>
-//         );
-//     }
-
-//     if (error) {
-//         return <div>Error: {error}</div>;
-//     }
-//     if (!blog) {
-//         return <div>Blog not found</div>;
-//     }
-
-//     // Check if blog is not undefined
-//     if (!blog) {
-//         return <div>Blog not found</div>;
-//     }
-
-//     const singleBlog = blog[0];
-
-//     return (
-//         <div>
-//             <Appbar />
-//             <FullBlog blog={singleBlog} />
-//         </div>
-//     );
-// };
-
-
 import { useBlog } from "../hooks";
 import { useParams } from "react-router-dom";
 import { FullBlog } from "../components/FullBlog";
@@ -58,27 +10,66 @@ export const Blog = () => {
         id: id || ""
     });
 
-    if (!blog || loading) {
-        return <div>
-            <Appbar />
-            <div className="h-screen flex flex-col justify-center">
-                <div className="flex justify-center">
-                    <Spinner />
+    if (loading || !blog) {
+        return (
+            <div>
+                <Appbar />
+                <div className="h-screen flex flex-col justify-center">
+                    <div className="flex justify-center">
+                        <Spinner />
+                    </div>
                 </div>
             </div>
-        </div>
+        );
     }
 
     if (error) {
-        return <div>
-            Error: {error}
-        </div>;
+        return <div>Error: {error}</div>;
     }
+
 
     return (
         <div>
+            <Appbar />
             <FullBlog blog={blog} />
         </div>
     );
 };
+
+
+// import { useBlog } from "../hooks";
+// import { useParams } from "react-router-dom";
+// import { FullBlog } from "../components/FullBlog";
+// import { Spinner } from "../components/Spinner";
+// import { Appbar } from "../components/Appbar";
+
+// export const Blog = () => {
+//     const { id } = useParams();
+//     const { loading, blog, error } = useBlog({
+//         id: id || ""
+//     });
+
+//     if (!blog || loading) {
+//         return <div>
+//             <Appbar />
+//             <div className="h-screen flex flex-col justify-center">
+//                 <div className="flex justify-center">
+//                     <Spinner />
+//                 </div>
+//             </div>
+//         </div>
+//     }
+
+//     if (error) {
+//         return <div>
+//             Error: {error}
+//         </div>;
+//     }
+
+//     return (
+//         <div>
+//             <FullBlog blog={blog} />
+//         </div>
+//     );
+// };
 
