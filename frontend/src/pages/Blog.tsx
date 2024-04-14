0d0f31c3d78c0729485c201b31127d5a11db0020
spidey0p <jyotiradityabiswal64@gmail.com>
2024-04-14 22:39:42 +0530

    final

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -30,46 +30,10 @@ export const Blog = () => {
 
     return (
         <div>
-            <Appbar />
+            {/* <Appbar /> */}
             <FullBlog blog={blog} />
         </div>
     );
 };
 
-
-// import { useBlog } from "../hooks";
-// import { useParams } from "react-router-dom";
-// import { FullBlog } from "../components/FullBlog";
-// import { Spinner } from "../components/Spinner";
-// import { Appbar } from "../components/Appbar";
-
-// export const Blog = () => {
-//     const { id } = useParams();
-//     const { loading, blog, error } = useBlog({
-//         id: id || ""
-//     });
-
-//     if (!blog || loading) {
-//         return <div>
-//             <Appbar />
-//             <div className="h-screen flex flex-col justify-center">
-//                 <div className="flex justify-center">
-//                     <Spinner />
-//                 </div>
-//             </div>
-//         </div>
-//     }
-
-//     if (error) {
-//         return <div>
-//             Error: {error}
-//         </div>;
-//     }
-
-//     return (
-//         <div>
-//             <FullBlog blog={blog} />
-//         </div>
-//     );
-// };
 
